481857c6bd7962880ebe4dd8b559fcc5a7fbec2e
Rodion <revan1495@mail.ru>
2022-06-06 13:27:00 +0700

    Initial commit

---
+++ app/src/main/java/com/example/petshelter/ui/GraphRoutes.kt
@@ -0,0 +1,6 @@
+package com.example.petshelter.ui
+
+const val notLoggedUserGraph = "notLoggedGraph"
+const val loggedUserGraph = "loggedGraph"
+const val userSettingsGraph = "userSettingsGraph"
+const val dataScreensGraph = "dataScreensGraph"
